3ec5a358a674b8e1483ed04ae96cd425453d60b0
Nathaniel Moschkin <precise.tester@gmail.com>
2022-06-27 16:37:36 -0400

    Update flask requirements and script

--- {{ cookiecutter.repo_name }}/src/run.py
+++ {{ cookiecutter.repo_name }}/src/run.py
@@ -2,7 +2,7 @@ import os
 from app import create_app
 from dotenv import load_dotenv
 
-dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
+dotenv_path = os.path.join(os.path.dirname(__file__), '../.env')
 
 #load env variables from .env file
 if os.path.exists(dotenv_path):
